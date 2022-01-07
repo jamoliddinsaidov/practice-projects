7a8ba1f17e8be14e8344d9af5a692825a9ecee83
Jamoliddin Saidov <sjamoliddin21@gmail.com>
2022-01-08 00:02:34 +0500

    finish the 4th challenge

--- 1-beginner/04_christmas_lights/app.js
+++ 1-beginner/04_christmas_lights/app.js
@@ -1,5 +1,8 @@
 const onBtn = document.querySelector('.on-btn')
 const offBtn = document.querySelector('.off-btn')
+const runBtn = document.querySelector('.run-btn')
+const rangeInput = document.querySelector('.range-input')
+const errorTxt = document.querySelector('.error-text')
 const lightbulbs = document.querySelectorAll('.lightbulb')
 const bgColors = [
   'bg-red-500',
@@ -22,17 +25,35 @@ const shadowColors = [
   'shadow-green-500',
 ]
 const defaultBgColor = 'bg-neutral-900'
+let range = 2
 let isOddTurn = true
+let isRunning = false
 let timerID
 
+// event listeners
 onBtn.addEventListener('click', turnOnLightbulbs)
 offBtn.addEventListener('click', turnOffLightbulbs)
+runBtn.addEventListener('click', () => {
+  let currRange = +rangeInput.value
 
+  if (currRange > 0 && currRange <= 5 && isRunning) {
+    range = currRange
+    stopAnimation()
+    startAnimation()
+  } else if (!isRunning) {
+    showErrorMsg('lights are off')
+  } else {
+    showErrorMsg('please enter numbers between 1 and 5')
+  }
+})
+
+// functions
 function turnOnLightbulbs() {
   lightbulbs.forEach((bulb, index) => {
     changeClasses(bulb, defaultBgColor, bgColors[index])
   })
 
+  isRunning = !isRunning
   startAnimation()
 }
 
@@ -42,6 +63,7 @@ function turnOffLightbulbs() {
     changeClasses(bulb, shadowColors[index])
   })
 
+  isRunning = !isRunning
   stopAnimation()
 }
 
@@ -58,7 +80,7 @@ function startAnimation() {
     }
 
     isOddTurn = !isOddTurn
-  }, 500)
+  }, range * 200)
 }
 
 function stopAnimation() {
@@ -76,4 +98,15 @@ function animateShadow(bulb, index) {
 function changeClasses(element, removedClass, addedClass) {
   if (removedClass) element.classList.remove(removedClass)
   if (addedClass) element.classList.add(addedClass)
+}
+
+function showErrorMsg(msg) {
+  errorTxt.innerText = msg
+  errorTxt.classList.remove('opacity-0')
+  errorTxt.classList.add('opacity-100')
+
+  setTimeout(() => {
+    errorTxt.classList.remove('opacity-100')
+    errorTxt.classList.add('opacity-0')
+  }, 2000)
 }
--- 1-beginner/04_christmas_lights/readme.md
+++ 1-beginner/04_christmas_lights/readme.md
@@ -1,6 +1,8 @@
 # Christmas Lights
 
+Demo can be found [here](https://sjamoliddin.github.io/practice-projects/1-beginner/04_christmas_lights/index.html)
+
 ## User Stories
 
 - [x] User can press a button to start and stop the display
-- [ ] User can change the interval of time controlling the change in intensity
+- [x] User can change the interval of time controlling the change in intensity
