b9c3bf964cb1e3a32c852fe458978f72584a2df6
Jamoliddin Saidov <sjamoliddin21@gmail.com>
2022-01-16 21:08:23 +0500

    complete 6th challenge

--- 1-beginner/06_countdown_timer/app.js
+++ 1-beginner/06_countdown_timer/app.js
@@ -1,2 +1,153 @@
-console.log('yoyyooyoyoy')
-// new Date(year, month, date, hours, minutes, seconds, ms)
+// selectors and vars
+const inputs = document.getElementsByTagName('input')
+const startBtn = document.querySelector('.start-btn')
+const daysTxt = document.querySelector('.days-text')
+const hoursTxt = document.querySelector('.hours-text')
+const minutesTxt = document.querySelector('.minutes-text')
+const secondsTxt = document.querySelector('.seconds-text')
+let eventDetails = { name: '', date: '', time: '' }
+let countdownDetails = { days: 0, hours: 0, minutes: 0, seconds: 0 }
+let timerID = 0
+
+// event listeners
+startBtn.addEventListener('click', startCountDown)
+
+// functions
+function startCountDown(e) {
+  e.preventDefault()
+  let isValid = validateValues()
+
+  if (isValid) {
+    // execute actions
+    setEventDetails(e)
+    calculateLeftDays()
+    renderTimeDetails()
+    showCountDownContainer()
+    countDown()
+
+    // stop the count down
+    setInterval(() => {
+      let { days, hours, minutes, seconds } = countdownDetails
+      if (days === 0 && hours === 0 && minutes === 0 && seconds === 0) {
+        clearInterval(timerID)
+      }
+    }, 950)
+  }
+}
+
+function setEventDetails(e) {
+  for (let { name, value } of inputs) {
+    eventDetails[name] = value
+  }
+}
+
+function validateValues() {
+  let isValid = false
+
+  if (!inputs[0].value) {
+    showErrorMsg('Please provide an event name')
+  } else if (!inputs[2].value || !inputs[1].value) {
+    showErrorMsg('Please provide a correct time period')
+  } else if (constructDate(inputs[1].value, inputs[2].value) <= new Date()) {
+    showErrorMsg('Please provide a date in the future')
+  } else {
+    isValid = true
+  }
+
+  return isValid
+}
+
+function constructDate(date, time) {
+  if (time) {
+    let [year, month, day] = splitDate(date)
+    let [hours, minutes] = time.split(':')
+    return new Date(year, month, day, hours, minutes)
+  }
+
+  let [year, month, day] = splitDate(date)
+  return new Date(year, month, day)
+}
+
+function splitDate(date) {
+  let [year, month, day] = date.split('-')
+  month = +month > 0 ? +month - 1 : month
+
+  return [year, month, day]
+}
+
+function calculateLeftDays() {
+  const { date, time } = eventDetails
+  const currentDate = new Date()
+  const futureDate = constructDate(date, time)
+
+  // calculate hours, mins, secs
+  let hoursDiff = futureDate.getHours() - currentDate.getHours()
+  let hours = hoursDiff < 0 ? 24 - Math.abs(hoursDiff) : hoursDiff
+
+  let minutesDiff = futureDate.getMinutes() - currentDate.getMinutes()
+  let minutes = minutesDiff
+
+  if (minutesDiff < 0) {
+    hours = hours - 1 > 0 ? hours - 1 : 0
+    minutes = 60 - Math.abs(minutesDiff)
+  }
+
+  let secondsDiff = futureDate.getSeconds() - currentDate.getSeconds()
+  let seconds = secondsDiff <= 0 ? 59 - Math.abs(secondsDiff) : secondsDiff
+
+  // calcuate days
+  let yearsDiff = futureDate.getFullYear() - currentDate.getFullYear()
+  let years = yearsDiff * 365
+
+  let monthsDiff = futureDate.getMonth() - currentDate.getMonth()
+  let months = monthsDiff * new Date(futureDate.getFullYear(), monthsDiff, 0).getDate()
+
+  let daysDiff = futureDate.getDate() - currentDate.getDate()
+  let days = years + months + daysDiff
+
+  if (hoursDiff <= 0 && minutesDiff < 0) {
+    days = days - 1 > 0 ? days - 1 : 0
+    hours = 23
+  }
+
+  // update values
+  countdownDetails = { days, hours, minutes, seconds }
+}
+
+function renderTimeDetails() {
+  let { days, hours, minutes, seconds } = countdownDetails
+
+  // set element content
+  daysTxt.innerText = days
+  hoursTxt.innerText = hours
+  minutesTxt.innerText = minutes
+  secondsTxt.innerText = seconds
+}
+
+function countDown() {
+  timerID = setInterval(() => {
+    calculateLeftDays()
+    renderTimeDetails()
+  }, 1000)
+}
+
+function showCountDownContainer() {
+  const eventTxt = document.querySelector('.event-text span')
+  const countdownContainer = document.querySelector('.countdown-container')
+
+  eventTxt.innerText = eventDetails.name
+  countdownContainer.classList.remove('opacity-0')
+}
+
+function showErrorMsg(msg) {
+  const errorTxt = document.querySelector('.error-text')
+
+  errorTxt.innerText = msg
+  errorTxt.classList.remove('opacity-0')
+  errorTxt.classList.add('opacity-100')
+
+  setTimeout(() => {
+    errorTxt.classList.remove('opacity-100')
+    errorTxt.classList.add('opacity-0')
+  }, 2000)
+}
