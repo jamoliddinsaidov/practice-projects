4c9b27b8f2944fee2207d7e5cff4715861d8340c
Jamoliddin Saidov <sjamoliddin21@gmail.com>
2022-06-05 14:25:22 +0500

    refactor & add read/get all functions

--- 1-beginner/09_markdown_notes_app/app.js
+++ 1-beginner/09_markdown_notes_app/app.js
@@ -1,4 +1,5 @@
-import { removeClass, addClass } from './utils.js'
+import { createNote, getAllNotes } from './crud.js'
+import { removeClass, addClass, getNoteTitle, createElement } from './utils.js'
 
 // Selectors
 const noteInput = document.querySelector('#note-input')
@@ -8,24 +9,31 @@ const formContainer = document.querySelector('form')
 const notesListContainer = document.querySelector('#notes-list-container')
 const notesPageLink = document.querySelector('#notes')
 const createPageLink = document.querySelector('#create')
+const formBtn = document.querySelector('#form-btn')
 
 // Event Listeners
 window.addEventListener('DOMContentLoaded', renderDefaultPage)
 noteInput.addEventListener('input', renderPreview)
+formBtn.addEventListener('click', submitForm)
 navLinks.forEach((navLink) => navLink.addEventListener('click', changePages))
 
 // Functions
 function renderDefaultPage(e) {
   const pageId = e.target.location.hash
 
-  if (pageId.includes('create')) renderCreatePage()
-  else if (pageId.includes('notes')) renderNotesListPage()
+  if (pageId.includes('notes')) renderNotesListPage()
+  else renderCreatePage()
 }
 
 function renderPreview(e) {
   const { value } = e.target
   const parsedValue = marked.parse(value)
   notePreview.innerHTML = parsedValue
+}
+
+function submitForm(e) {
+  e.preventDefault()
+  createNote(noteInput.value)
 }
 
 function changePages(e) {
@@ -47,4 +55,26 @@ function renderNotesListPage() {
   removeClass(notesListContainer, 'hide')
   addClass(formContainer, 'hide')
   addClass(notesPageLink, 'active-link')
+
+  renderNotesList()
+}
+
+function renderNotesList() {
+  const notesListContainer = document.querySelector('#notes-list')
+  const notes = getAllNotes()
+
+  if (notesListContainer.childElementCount === notes.length) return
+
+  notesListContainer.innerHTML = ''
+  notes.forEach(({ noteText, id }) => {
+    const noteTitle = getNoteTitle(noteText)
+
+    const noteElement = `<li>
+                          <p> ${noteTitle} </p>
+                          <div>
+                          </div>
+                        </li>`
+
+    notesListContainer.appendChild(noteElement)
+  })
 }
